e17db9d0969274afdedcf74a03d8e3d83245d8b5
Michael Zhong <zhongmic@umich.edu>
2019-03-01 14:52:07 -0500

    Code clean up and comments

--- excel2arduino_glowsuit.py
+++ excel2arduino_glowsuit.py
@@ -1,208 +1,16 @@
+'''Python script to convert csv data to .ino file.'''
+
 import os
 import string
 import csv
 import sys
 import shutil
 
-# Forever
-FOREVER = 500000
+from code_templates import LOOP_BEGIN, LOOP_END, SETUP, LIGHTS_FUNCTIONS
+from config import *
 
-# Configuration for time
-TIME_ABS_COL = 1
-# Preset column (1st one if multiple)
-PRESET_COL = 5
 
-# Ordering of light bool values (distance from preset)
-LA = 1
-RA = 2
-LL = 3
-RL = 4
-HB = 5
-WB = 6
-YP = 7
-RP = 8
-
-# Number of columns dedicated to non-present information
-NUM_HEADER = 5
-# Number of columns dedicated to glowsuit settings
-NUM_SETTINGS = 9
-
-# Code strings
-LOOP_BEGIN = '''
-void loop() {
-'''
-
-LOOP_END = '''
-  //Ending delay, so stays off "forever"
-  lights_ALL_OFF(50000);
-}
-'''
-
-SETUP = '''
-void setup() {
-    // put your setup code here, to run once:
-    pinMode(2, OUTPUT);  // channel A - Left Arm
-    pinMode(3, OUTPUT);  // channel B - Right Arm
-    pinMode(4, OUTPUT);  // channel C - Left Leg
-    pinMode(5, OUTPUT);  // channel D - Right Leg
-    pinMode(6, OUTPUT);  // channel E - Head / Back
-    pinMode(7, OUTPUT);  // channel F - White Bottom
-    pinMode(8, OUTPUT);  // channel G - Yellow P
-    pinMode(9, OUTPUT);  // channel H - Red P
-    pinMode(13, OUTPUT);
-    digitalWrite(13, 1);
-    digitalWrite(2, 0);
-    digitalWrite(3, 0);
-    digitalWrite(4, 0);
-    digitalWrite(5, 0);
-    digitalWrite(6, 0);
-    digitalWrite(7, 0);
-    digitalWrite(8, 0);
-    digitalWrite(9, 0);
-}
-'''
-
-LIGHTS_FUNCTIONS = '''
-// Delay in ms
-void lights(bool A, bool B, bool C, bool D, bool E, bool F, bool G, bool H, int delay_ms) {
-    digitalWrite(2, A);
-    digitalWrite(3, B);
-    digitalWrite(4, C);
-    digitalWrite(5, D);
-    digitalWrite(6, E);
-    digitalWrite(7, F);
-    digitalWrite(8, G);
-    digitalWrite(9, H);
-    delay(delay_ms);
-}
-
-// All lights off
-void lights_ALL_OFF(int delay_ms) {
-    lights(0, 0, 0, 0, 0, 0, 0, 0, delay_ms);
-}
-
-// Everything on w/ Yellow P (no red P)
-void lights_ALL_ON_Yel_P(int delay_ms) {
-    lights(1, 1, 1, 1, 1, 1, 1, 0, delay_ms);
-}
-
-// Everything on w/ Red P (no yellow P)
-void lights_ALL_ON_Red_P(int delay_ms) {
-    lights(1, 1, 1, 1, 1, 1, 0, 1, delay_ms);
-}
-
-// Everything on but the Ps
-void lights_NO_Ps(int delay_ms) {
-    lights(1, 1, 1, 1, 1, 1, 0, 0, delay_ms);
-}
-
-// Just Red P
-void lights_Just_Red_P(int delay_ms) {
-    lights(0, 0, 0, 0, 0, 0, 0, 1, delay_ms);
-}
-
-// Just Yel P
-void lights_Just_Yel_P(int delay_ms) {
-    lights(0, 0, 0, 0, 0, 0, 1, 0, delay_ms);
-}
-
-void lights_Just_Both_P(int delay_ms) {
-    lights(0, 0, 0, 0, 0, 0, 1, 1, delay_ms);
-}
-
-// Just left arm
-void lights_Just_LeftArm(int delay_ms) {
-    lights(1, 0, 0, 0, 0, 0, 0, 0, delay_ms);
-}
-
-// Both Arms illuminated
-void lights_BOTH_ARMS(int delay_ms) {
-    lights(1, 1, 0, 0, 0, 0, 0, 0, delay_ms);
-}
-
-// Both legs illuminated
-void lights_BOTH_LEGS(int delay_ms) {
-    lights(0, 0, 1, 1, 0, 0, 0, 0, delay_ms);
-}
-
-// 1 second count of body flicker
-void The_Middle_Flicker() {
-    lights_BOTH_LEGS(200);
-    lights(0, 0, 0, 0, 0, 1, 0, 0, 200);
-    lights_Just_Yel_P(200);
-    lights_BOTH_ARMS(200);
-    lights(0, 0, 0, 0, 1, 0, 0, 0, 200);
-}
-
-// Flicker 2 seconds on Hypnotic
-void lights_Vintage_TShirt() {
-    lights(1, 1, 1, 1, 1, 0, 1, 0, 200);
-    lights(1, 1, 1, 1, 1, 1, 0, 0, 200);
-    lights(1, 1, 1, 1, 1, 0, 1, 0, 200);
-    lights(1, 1, 1, 1, 1, 1, 0, 0, 200);
-    lights(1, 1, 1, 1, 1, 0, 1, 0, 200);
-    lights(1, 1, 1, 1, 1, 1, 0, 0, 200);
-    lights(1, 1, 1, 1, 1, 0, 1, 0, 200);
-    lights(1, 1, 1, 1, 1, 1, 0, 0, 200);
-    lights(1, 1, 1, 1, 1, 0, 1, 0, 200);
-    lights(1, 1, 1, 1, 1, 1, 0, 0, 200);
-}
-
-// 1500 ms of body flickering
-void lights_Body_Flicker_2100ms() {
-    lights_Just_Yel_P(150);
-    lights_ALL_OFF(150);
-    lights_Just_Yel_P(150);
-    lights_ALL_OFF(150);
-    lights_Just_Yel_P(150);
-    lights_ALL_OFF(150);
-    lights_Just_Yel_P(150);
-    lights_ALL_OFF(150);
-    lights_Just_Yel_P(150);
-    lights_ALL_OFF(150);
-    lights_Just_Yel_P(100);
-    lights_ALL_OFF(100);
-    lights_Just_Yel_P(100);
-    lights_ALL_OFF(100);
-    lights_Just_Yel_P(100);
-    lights_ALL_OFF(100);
-}
-
-// 1000 ms of body flickering
-void lights_Body_Flicker_1000ms() {
-    lights_NO_Ps(100);
-    lights_ALL_OFF(100);
-    lights_NO_Ps(100);
-    lights_ALL_OFF(100);
-    lights_NO_Ps(100);
-    lights_ALL_OFF(100);
-    lights_NO_Ps(100);
-    lights_ALL_OFF(100);
-    lights_NO_Ps(100);
-    lights_ALL_OFF(100);
-}
-
-// 1500ms of flickering out
-void lights_END_Flicker_1500ms() {
-    lights_Just_Yel_P(100); //1
-    lights_Just_Both_P(100);
-    lights_Just_Red_P(100);
-    lights_Just_Yel_P(100); //2
-    lights_Just_Both_P(100);
-    lights_Just_Red_P(100);
-    lights_Just_Yel_P(100); //3
-    lights_Just_Both_P(100);
-    lights_Just_Red_P(100);
-    lights_Just_Yel_P(100); //4
-    lights_Just_Both_P(100);
-    lights_Just_Red_P(100);
-    lights_Just_Yel_P(100); //5
-    lights_Just_Both_P(100);
-    lights_Just_Red_P(100);
-}
-'''
-
-# Translate suit number to desired column
+# Translate suit number to desired column number
 def preset(suit_num=0):
     return PRESET_COL + suit_num * NUM_SETTINGS
 
@@ -232,11 +40,13 @@ def red_P(suit_num=0):
 
 
 def preset2code(preset_name, duration):
+    '''Convert a preset name to function call.'''
     function_call = "  lights_" + preset_name + "(" + str(duration) + ");\n"
     return function_call
 
 
 def lights2code(left_arm,right_arm,left_leg,right_leg,head_back,white_bottom,yellow_p,red_p,duration):
+    '''Convert boolean settings to lights function call.'''
     return "  lights(" +\
     left_arm      +"," +\
     right_arm     +"," +\
@@ -255,6 +65,7 @@ if __name__ == "__main__":
         print("Usage: python excel2arduino_glowsuit.py excel_filename")
         exit(1)
 
+    # Take filename from command line argument
     filename = sys.argv[1]
 
     # Open csv file
@@ -266,19 +77,33 @@ if __name__ == "__main__":
     # Array to store a loop string for each glowsuit
     ino_loops = []
 
-    # Initialize all glowsuit loops with loop begin
+    # Initialize all glowsuit code with loop begin
     for suit in range(num_suits):
         ino_loops.append(LOOP_BEGIN)
     
+    # Jump to next row, saving current row as prev_row
     prev_row = next(csvreader, None)
     prev_abs_time = prev_row[TIME_ABS_COL]
-    # Loop over every row in the CSV
-    for row in csvreader:
+    # Loop over remaining row in the csv
+    for idx,row in enumerate(csvreader):
 
-        # calculate the delay in milliseconds from previous lights
-        delay_ms = int(row[TIME_ABS_COL]) - int(prev_abs_time)
+        # calculate the delay in milliseconds from previous row
+
+        try:
+            delay_ms = int(row[TIME_ABS_COL]) - int(prev_abs_time)
+            if (delay_ms < 0):
+                print("Error: negative delay value near row", idx+3)
+                csvfile.close()
+                exit(1)
+        except ValueError as err:
+            print(err)
+            print("Hint: Value in Time Abs column is undefined or invalid near row", idx+3)
+            csvfile.close()
+            exit(1)
+
+
         for suit in range(num_suits):
-            # Preset is set
+            # Preset is chosen
             if (prev_row[preset(suit)] != ""):
                 ino_loops[suit] += preset2code(prev_row[preset(suit)], delay_ms)
             else:
@@ -295,7 +120,7 @@ if __name__ == "__main__":
                                     )
         # Set prev_row as current row
         prev_row = row
-        prev_abs_time = int(row[TIME_ABS_COL])
+        prev_abs_time = row[TIME_ABS_COL]
 
     # Final row should have 'infinite' delay
     for suit in range(num_suits):
@@ -313,6 +138,9 @@ if __name__ == "__main__":
                                     prev_row[red_P(suit)],
                                     FOREVER
                                 )
+
+    # Close csv file
+    csvfile.close()
 
     # Close all glowsuit loops with loop end
     for suit in range(num_suits):
